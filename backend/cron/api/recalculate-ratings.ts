29ebac6ed3d703f8ef1a685d46fbb994cb3087ff
Fareskingtube <firoobasha@gmail.com>
2026-08-27 21:19:01 +0300

    Cron 3.0

--- backend/cron/api/recalculate-ratings.ts
+++ backend/cron/api/recalculate-ratings.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { PrismaClient as PostgresClient } from '../../src/generated/prisma-postgres';
+import { PrismaClient as PostgresClient } from '../../../backend/src/generated/prisma-postgres';
 import { MongoClient } from 'mongodb';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
